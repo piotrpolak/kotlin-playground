6d3076071263ff3f4435df1af71da2ff75e0ee5c
Piotr Polak <piotr@polak.ro>
2019-03-19 12:44:43 +0100

    Extracted JacksonBrokenIsPrefixTest

--- src/test/kotlin/ro/polak/kotlin/playground/JacksonTest.kt
+++ src/test/kotlin/ro/polak/kotlin/playground/JacksonTest.kt
@@ -1,13 +1,12 @@
 package ro.polak.kotlin.playground
 
-import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import ro.polak.playground.jackson.*
+import ro.polak.playground.jackson.JsonAnyHolder
 
 
 class JacksonTest {
@@ -39,57 +38,5 @@ class JacksonTest {
 
         @Suppress("UNCHECKED_CAST")
         assertThrows<ClassCastException> { (output.result as List<String>) }
-    }
-
-    // That is sad :(
-    @Test
-    fun shouldBreakNoAnnotation() {
-        assertThrows<UnrecognizedPropertyException> { mapper.readValue<JsonIsPrefixedBroken>("{\"isBroken\": true}") }
-
-        assertThrows<UnrecognizedPropertyException> { mapper.readValue<JsonIsPrefixedBroken>("{\"broken\": true}") }
-
-        assertEquals("{\"broken\":true}", mapper.writeValueAsString(JsonIsPrefixedBroken(true)))
-    }
-
-    @Test
-    fun shouldHandleGetAnnotated() {
-        mapper.readValue<JsonIsPrefixedBrokenGetAnnotated>("{\"isBrokenAnnotated\": true}").run {
-            assertEquals(true, isBrokenAnnotated)
-        }
-
-        assertEquals("{\"isBrokenAnnotated\":true}", mapper.writeValueAsString(JsonIsPrefixedBrokenGetAnnotated(true)))
-    }
-
-    // That is redundancy
-    @Test
-    fun shouldHandleGetSetAnnotated() {
-        mapper.readValue<JsonIsPrefixedBrokenGetSetAnnotated>("{\"isBrokenAnnotated\": true}").run {
-            assertEquals(true, isBrokenAnnotated)
-        }
-
-        assertEquals("{\"isBrokenAnnotated\":true}", mapper.writeValueAsString(JsonIsPrefixedBrokenGetSetAnnotated(true)))
-    }
-
-    @Test
-    fun shouldHandleFieldAnnotation() {
-        mapper.readValue<JsonIsPrefixedBrokenFieldAnnotated>("{\"isBrokenAnnotated\": true}").run {
-            assertEquals(true, isBrokenAnnotated)
-        }
-
-        // That is sad :(
-        assertEquals(
-            "{\"isBrokenAnnotated\":true,\"brokenAnnotated\":true}",
-            mapper.writeValueAsString(JsonIsPrefixedBrokenFieldAnnotated(true))
-        )
-    }
-
-    @Test
-    fun shouldHandleGenericAnnotation() {
-        mapper.readValue<JsonIsPrefixedBrokenAnnotated>("{\"isBrokenAnnotated\": true}").run {
-            assertEquals(true, isBrokenAnnotated)
-        }
-
-        // That is sad :(
-        assertEquals("{\"brokenAnnotated\":true}", mapper.writeValueAsString(JsonIsPrefixedBrokenAnnotated(true)))
     }
 }
